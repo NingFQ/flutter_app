a689a6b566579ceb14ae540ce09c27315c70784f
ningfangqiang <17600407754@163.com>
2021-05-28 10:10:06 +0800

    button

---
+++ lib/common/widgets/gradient_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class GradientButton extends StatelessWidget {
+  final double height;
+  final List<Color> colors;
+  final double width;
+  final Widget child;
+  final GestureTapCallback onPressed;
+
+  const GradientButton(
+      {Key key,
+      this.width,
+      this.height,
+      this.colors,
+      this.onPressed,
+      @required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    List<Color> _colors = colors ?? [theme.primaryColor,theme.primaryColorDark ?? theme.primaryColor];
+
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: _colors),
+        borderRadius: BorderRadius.all(Radius.circular(10))
+      ),
+      child: Material(
+        type: MaterialType.transparency,
+        child: ConstrainedBox(
+          constraints: BoxConstraints.tightFor(height: height, width: width),
+          child: Center(
+            child: Text('Button',style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30
+            ),),
+          ),
+        ),
+      ),
+    );
+  }
+}
