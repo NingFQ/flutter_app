662cc3ac02e0808899c7029a26502fffaf2d156a
ningfangqiang <17600407754@163.com>
2021-06-29 10:04:59 +0800

    socket
    提交测试

--- lib/page/demo/web_socket.dart
+++ lib/page/demo/web_socket.dart
@@ -9,33 +9,69 @@ class WebSocketTest extends StatefulWidget {
 }
 
 class _WebSocketTestState extends State<WebSocketTest> {
-  final channel = IOWebSocketChannel.connect('ws://echo.websocket.org');
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    channel.sink.add('Hello!');
-  }
+  TextEditingController _controller = new TextEditingController();
+  IOWebSocketChannel channel;
+  String _text = "";
+
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    channel.sink.close();
+  void initState() {
+    //创建websocket连接
+    channel = new IOWebSocketChannel.connect('ws://echo.websocket.org');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Web Socket'),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("WebSocket(内容回显)"),
       ),
-      body: new StreamBuilder(
-        stream: channel.stream,
-        builder: (context, snapshot) {
-          return new Text(snapshot.hasData ? '${snapshot.data}' : 'aaa');
-        },
+      body: new Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Form(
+              child: new TextFormField(
+                controller: _controller,
+                decoration: new InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            new StreamBuilder(
+              stream: channel.stream,
+              builder: (context, snapshot) {
+                //网络不通会走到这
+                if (snapshot.hasError) {
+                  _text = "网络不通...";
+                } else if (snapshot.hasData) {
+                  _text = "echo: "+snapshot.data;
+                }
+                return new Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: new Text(_text),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: new Icon(Icons.send),
       ),
     );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    super.dispose();
   }
 }
